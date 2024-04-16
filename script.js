0e0c23bee12157afad80b75d151a76198901a0d3
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-04-16 18:45:55 +0530

    Restructuring, error check, and loop
    Restructured some variables to make them global
    
    Added an if-else condition on the overall output to check if the user input is valid or not.
    
    Added loop to play the game 5 times and then calculate the overall result at the end.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// Global variables
+// Global variables and inputs
 
 let playerWins = 0, computerWins = 0;
+
+let playerSelection = prompt("Please enter your choice out of Rock, Paper, and Scissors");
+
+let player = playerSelection.toUpperCase();
 
 // Function to get the random choice taken by the computer
 
@@ -9,44 +13,74 @@ function getComputerChoice() {
 
     switch(choiceNum) {
         case 0:
-            return "Rock";
+            return "ROCK";
         
         case 1:
-            return "Paper";
+            return "PAPER";
         
         case 2:
-            return "Scissors";
+            return "SCISSORS";
     }
 }
 
 // Function to play a single round
 
 function playRound(playerSelection, computerSelection) {
-    let player = playerSelection.toUpperCase();
-    let computer = computerSelection.toUpperCase();
-
-    console.log(`Computer chose ${computer} and you chose ${player}`);
 
     switch(true) {
-        case player === "ROCK" && computer === "SCISSORS":
-        case player === "PAPER" && computer === "ROCK":
-        case player === "SCISSORS" && computer === "PAPER":
+        case player === "ROCK" && computerSelection === "SCISSORS":
+        case player === "PAPER" && computerSelection === "ROCK":
+        case player === "SCISSORS" && computerSelection === "PAPER":
             playerWins++;
-            return `You Won! ${player} beats ${computer}`;
+            return `You Won! ${player} beats ${computerSelection}`;
         
-        case player === computer:
+        case player === computerSelection:
             return "It's a tie!"
         
         default:
             computerWins++;
-            return `You Lose! ${computer} beats ${player}`;
+            return `You Lose! ${computerSelection} beats ${player}`;
     }
 
 }
 
-let playerSelection = "rock";
+// Loop to play 5 times
 
-// let playerSelection = prompt("Please enter your choice out of Rock, Paper, and Scissors");
-console.log(playRound(playerSelection, getComputerChoice()));
-console.log(`Player wins = ${playerWins}
-Computer wins = ${computerWins}`);
+function playGame() {
+    for (let i=0; i<5; i++) {
+        console.log(playRound(playerSelection, getComputerChoice()));
+    }
+}
+
+// Making sure input is valid
+
+if (player === "ROCK" || player === "PAPER" || player === "SCISSORS") {
+    
+    // Invoke the loop and play 5 times
+
+    playGame();
+
+    // Print the final calculated result
+    
+    if (playerWins > computerWins) {
+        console.log(`RESULT: Player you won the entire game!
+        Your score: ${playerWins}
+        Computer's Score: ${computerWins}`);
+    }
+
+    else if (playerWins === computerWins) {
+        console.log(`RESULT: It's an overall tie!
+        Your score: ${playerWins}
+        Computer's Score: ${computerWins}`);
+    }
+
+    else {
+        console.log(`RESULT: Computer won the entire game!
+        Your score: ${playerWins}
+        Computer's Score: ${computerWins}`);
+    }
+}
+
+else {
+    console.log("Wrong input, please enter again");
+}
